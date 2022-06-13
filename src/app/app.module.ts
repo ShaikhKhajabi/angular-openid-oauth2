83f4938fa72dc4322419a5803051df90f0b91fac
Khajabi Shaikh <khajabi0410@gmail.com>
2022-06-13 14:31:26 +0530

    main components

---
+++ src/app/app.module.ts
@@ -0,0 +1,60 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './home/contact-us.component';
+import { ProjectListComponent } from './projects/project-list.component';
+import { ProjectComponent } from './projects/project.component';
+import { AddEditMilestoneDialogComponent } from './projects/add-edit-milestone-dialog.component';
+
+import { CoreModule } from './core/core.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AdminModule } from './admin/admin.module';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ContactUsComponent,
+    ProjectListComponent,
+    ProjectComponent,
+    AddEditMilestoneDialogComponent,
+
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    AdminModule,
+    CoreModule,
+    AppRoutingModule
+  ],
+  providers: [
+  ],
+  bootstrap: [AppComponent],
+  entryComponents:[
+    AddEditMilestoneDialogComponent
+  ]
+})
+export class AppModule { }
