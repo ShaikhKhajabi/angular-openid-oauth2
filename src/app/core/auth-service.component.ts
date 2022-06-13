834abaa66ae5b95b2b42b2eae2968f4d2ca6bb79
Khajabi Shaikh <khajabi0410@gmail.com>
2022-06-13 16:32:32 +0530

    created new auth-service file added oidc-client and configure oidc to sts

---
+++ src/app/core/auth-service.component.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { User, UserManager } from 'oidc-client';
+import { Constants } from '../constants';
+import { CoreModule } from './core.module';
+
+@Injectable({ providedIn: CoreModule })
+export class AuthService {
+
+    private _userManager: UserManager;
+    private _user!: User;
+
+
+    constructor() { 
+        const stSettings ={
+            authority: Constants.stsAuthority,
+            client_id: Constants.clientId,
+            redirect_uri: `${Constants.clientRoot}signin-callback`,
+            scope!: 'openid profile projects-api',
+            response_type: 'code',
+            post_logout_redirect_uri:`${Constants.clientRoot}signout-callback`,
+        };
+        this._userManager = new UserManager(stSettings);
+     }
+
+}
